9c89d8eb6eb76a4083b5979db41b6f1272154fcd
Anwyll Le <lelinhnguyen2109@gmail.com>
2023-05-18 00:18:58 -0400

    finsihed the pitch

--- src/components/Banner/About.jsx
+++ src/components/Banner/About.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import classes from "../../styles/Banner/About.module.css";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className={`w-11/12 lg:w-4/5 rounded-md p-5 mt-24 mx-auto ${classes.container}`}>
+      <h2 className="text-3xl">
+        About <span className="text-mainColor">me</span>
+      </h2>
+      <p>Hello, I'm <span className="text-mainColor">Linh Nguyen Le</span>, 5th semester student at Seneca College majoring in Computer Programming & Analysis. I'm a dedicated and team-oriented student searching for a co-op opportunity in software development. During my study, I've had the opportunity to work on various group projects where I collaborated with diverse teams to develop appropriate solutions. This experience has honed my communication, collaboration, and project management skills. Recently, I and my team got to the final round of our school's hackathon. I'm also familiar with programming languages like JavaScript, C/C++ and Python as well as web technologies such as React, Next.js, Tailwinds and MongoDB. With my work ethic, and passion for problem-solving, I'm eager to make a positive impact and apply my knowledge into real-world settings.</p>
+    </div>
+  );
 }
 
-export default About
+export default About;
