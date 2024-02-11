566614c216ca3d583f57509cc78d7f9777948d6b
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-02-11 14:08:22 -0500

    updated timeline responsiveness

--- src/components/Activities/Timeline.jsx
+++ src/components/Activities/Timeline.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Fade, Slide, Zoom } from "react-awesome-reveal";
 
 const ReadMore = ({ children }) => {
   const text = children;
@@ -30,74 +31,76 @@ const ReadMore = ({ children }) => {
 function Timeline(props) {
   return (
     <div className="flex items-center justify-center text-stone-200">
-      <ol className="items-center list-none w-full">
+      <ol className=" list-none w-full">
         {props.activities.map((activity, index) => {
           return (
-            <li className="relative flex ">
-              <div className=" w-1/2 relative flex flex-col items-center justify-center px-3">
-                {index % 2 !== 0 && (
-                  <div>
-                    <h3 className="text-lg font-bold text-mainColor">
-                      {activity.cardTitle}
-                    </h3>
-                    <h4 className="text-base mx-0.5 font-bold">
-                      {activity.title}
-                    </h4>
-                  </div>
-                )}
-                {index % 2 === 0 && (
-                  <div className="flex flex-col items-center justify-center bg-stone-800 overflow-hidden rounded-2xl ">
-                    <div className="w-full overflow-hidden rounded-2xl mb-3 ">
-                      <img
-                        src={activity.media.source.url}
-                        alt={activity.media.name}
-                        className="h-full w-full"
-                      ></img>
+            <Zoom triggerOnce>
+              <li className="relative flex ">
+                <div className=" w-1/2 relative flex flex-col items-center justify-center pr-3">
+                  {index % 2 !== 0 && (
+                    <div>
+                      <h3 className="text-lg font-bold text-mainColor">
+                        {activity.cardTitle}
+                      </h3>
+                      <h4 className="text-base mx-0.5 font-bold">
+                        {activity.title}
+                      </h4>
                     </div>
-                    <div className="mx-2 mb-2 relative">
-                      <p className="font-bold text-mainColor">
-                        {activity.cardSubtitle}
-                      </p>
-                      <ReadMore>{activity.cardDetailedText}</ReadMore>
+                  )}
+                  {index % 2 === 0 && (
+                    <div className="flex flex-col items-center justify-center bg-stone-800 overflow-hidden rounded-2xl ">
+                      <div className="w-full overflow-hidden rounded-2xl mb-3 ">
+                        <img
+                          src={activity.media.source.url}
+                          alt={activity.media.name}
+                          className="h-full w-full"
+                        ></img>
+                      </div>
+                      <div className="mx-2 mb-2 relative">
+                        <p className="font-bold text-mainColor">
+                          {activity.cardSubtitle}
+                        </p>
+                        <ReadMore>{activity.cardDetailedText}</ReadMore>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </div>
-              <div className="flex flex-col items-center">
-                <div className=" h-full bg-mainColor w-0.5 0"></div>
-                <div className="z-10 w-5 h-5 bg-mainColor rounded-full ring-4 ring-gray-600 shrink-0"></div>
-                <div className=" h-full bg-mainColor w-0.5 0"></div>
-              </div>
-              <div className="mt-3 w-1/2 flex flex-col items-center justify-center px-3">
-                {index % 2 === 0 && (
-                  <div>
-                    <h3 className="text-lg font-bold text-mainColor">
-                      {activity.cardTitle}
-                    </h3>
-                    <h4 className="text-base mx-0.5 font-bold">
-                      {activity.title}
-                    </h4>
-                  </div>
-                )}
-                {index % 2 !== 0 && (
-                  <div className="flex flex-col items-center justify-center bg-stone-800 overflow-hidden rounded-2xl">
-                    <div className="w-full overflow-hidden rounded-2xl mb-3 ">
-                      <img
-                        src={activity.media.source.url}
-                        alt={activity.media.name}
-                        className="h-full w-full"
-                      ></img>
+                  )}
+                </div>
+                <div className="flex flex-col items-center">
+                  <div className=" h-full bg-mainColor w-0.5 0"></div>
+                  <div className="z-10 w-5 h-5 bg-mainColor rounded-full ring-4 ring-gray-600 shrink-0"></div>
+                  <div className=" h-full bg-mainColor w-0.5 0"></div>
+                </div>
+                <div className=" w-1/2 flex flex-col items-center justify-center pl-3">
+                  {index % 2 === 0 && (
+                    <div>
+                      <h3 className="text-lg font-bold text-mainColor">
+                        {activity.cardTitle}
+                      </h3>
+                      <h4 className="text-base mx-0.5 font-bold">
+                        {activity.title}
+                      </h4>
                     </div>
-                    <div className="mx-2 mb-2 relative">
-                      <p className="font-bold text-mainColor">
-                        {activity.cardSubtitle}
-                      </p>
-                      <ReadMore>{activity.cardDetailedText}</ReadMore>
+                  )}
+                  {index % 2 !== 0 && (
+                    <div className="flex flex-col items-center justify-center bg-stone-800 overflow-hidden rounded-2xl">
+                      <div className="w-full overflow-hidden rounded-2xl mb-3 ">
+                        <img
+                          src={activity.media.source.url}
+                          alt={activity.media.name}
+                          className="h-full w-full"
+                        ></img>
+                      </div>
+                      <div className="mx-2 mb-2 relative">
+                        <p className="font-bold text-mainColor">
+                          {activity.cardSubtitle}
+                        </p>
+                        <ReadMore>{activity.cardDetailedText}</ReadMore>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </div>
-            </li>
+                  )}
+                </div>
+              </li>
+            </Zoom>
           );
         })}
       </ol>
